307b82993c5c7ff8dd98bb5d0e0e54f10ac87716
kkyfd <l20f21@outlook.com.br>
2024-09-19 21:34:17 -0300

    Second rainstorm backup

--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         character: Character::load_from_file("characters.json").unwrap_or_default(),
     };
     let options = eframe::NativeOptions {
-        min_size: Some(egui::vec2(800.0, 600.0)), 
-        ..Default::default() 
+        viewport: egui::ViewportBuilder {
+            inner_size: Some(egui::vec2(800.0, 650.0)),
+            ..Default::default()
+        },
+        ..Default::default()
     };
     let _ = eframe::run_native(
         "BA Reports",
-        eframe::NativeOptions::default(),
-        Box::new(|cc| Ok(Box::new(AppState::new(cc, wrapper.reports, wrapper.character)) as Box<dyn App>)),
+        options,
+        Box::new(|cc| {
+            Ok(Box::new(AppState::new(cc, wrapper.reports, wrapper.character)) as Box<dyn App>)
+        }),
     );
     Ok(())
 }
@@ -84,91 +89,109 @@ impl AppState {
 impl App for AppState {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                // Reports Group
-                ui.group(|ui| {
-                    let labels = [
-                        ("White Reports", "white_report"),
-                        ("Blue Reports", "blue_report"),
-                        ("Orange Reports", "orange_report"),
-                        ("Purple Reports", "purple_report"),
-                    ];
-                    // Reports Text Boxes
-                    ui.vertical(|ui| {
-                        ui.heading("Reports Amount");
-                        for (i, (label, key)) in labels.iter().enumerate() {
-                            ui.horizontal(|ui| {
-                                if let Some(texture) = self.textures.get(*key) {
-                                    ui.image((texture.id(), egui::Vec2::new(120.0, 120.0)));
-                                }
-                                let mut quantity_str = self.reports.quantities[i].to_string();
+            // Create a vertical layout for the main content
+            ui.vertical(|ui| {
+                ui.horizontal(|ui| {
+                    // Reports Group
+                    ui.group(|ui| {
+                        let labels = [
+                            ("White Reports", "white_report"),
+                            ("Blue Reports", "blue_report"),
+                            ("Orange Reports", "orange_report"),
+                            ("Purple Reports", "purple_report"),
+                        ];
+                        // Reports Text Boxes
+                        ui.vertical(|ui| {
+                            ui.heading("Reports Amount");
+                            for (i, (label, key)) in labels.iter().enumerate() {
+                                ui.horizontal(|ui| {
+                                    if let Some(texture) = self.textures.get(*key) {
+                                        ui.image((texture.id(), egui::Vec2::new(120.0, 120.0)));
+                                    }
+                                    let mut quantity_str = self.reports.quantities[i].to_string();
+                                    ui.add(
+                                        egui::TextEdit::singleline(&mut quantity_str)
+                                            .desired_width(50.0),
+                                    );
+                                    if let Ok(value) = quantity_str.parse::<f32>() {
+                                        self.reports.quantities[i] = value;
+                                    }
+                                    ui.label(format!("{}: {}", label, self.reports.quantities[i]));
+                                });
+                            }
+                        });
+                    });
+
+                    // Characters Group
+                    ui.group(|ui| {
+                        let mut desired_level = String::with_capacity(2);
+                        ui.vertical(|ui| {
+                            if let None = &self.character.name {
                                 ui.add(
-                                    egui::TextEdit::singleline(&mut quantity_str)
-                                        .desired_width(50.0),
+                                    egui::TextEdit::singleline(&mut desired_level).desired_width(50.0),
                                 );
-                                if let Ok(value) = quantity_str.parse::<f32>() {
-                                    self.reports.quantities[i] = value;
-                                }
-                                ui.label(format!("{}: {}", label, self.reports.quantities[i]));
-                            });
-                        }
+                            }
+                            if let Ok(level) = desired_level.parse::<u8>() {
+                                desired_level = level.to_string();
+                            }
+                            if let (Some(name), Some(level)) =
+                                (&self.character.name, &self.character.level)
+                            {
+                                ui.heading("Character Details");
+                                ui.label(format!("{:?}", name));
+                                ui.label(format!("Level: {}", level));
+                                ui.heading(format!("Inputs"));
+                                ui.add(
+                                    egui::TextEdit::singleline(&mut desired_level).desired_width(50.0),
+                                );
+                            }
+                        });
                     });
                 });
 
-                // Characters Group
-                if let (Some(name), Some(level)) = (&self.character.name, &self.character.level) {
-                    ui.vertical(|ui|{
-                        ui.group(|ui| {
-                            ui.heading("Character Details");
-                            ui.label(format!("Character Name: {:?}", name));
-                            ui.label(format!("Level: {}", level));
-                        });
-                    });
-                    
+                // Buttons Group (placed below both groups)
+                ui.horizontal(|ui| {
+                    if ui.button("Convert").clicked() {
+                        let purple_reports = (self.reports.quantities[0] / 200.0)
+                            + (self.reports.quantities[1] / 20.0)
+                            + (self.reports.quantities[2] / 5.0)
+                            + self.reports.quantities[3];
+                        let exp = purple_reports * 10000.0;
+                        self.reports.purple_reports = Some(purple_reports);
+                        self.reports.exp = Some(exp);
+                    }
+
+                    if ui.button("Clear").clicked() {
+                        self.reports.purple_reports = None;
+                        self.reports.exp = None;
+                    }
+
+                    if ui.button("Save").clicked() {
+                        let reports_path = "reports.json";
+                        let character_path = "characters.json";
+                        if let Err(err) = self.reports.save_to_file(reports_path) {
+                            eprintln!("Failed to save reports: {}", err);
+                        }
+                        if let Err(err) = self.character.save_to_file(character_path) {
+                            eprintln!("Failed to save characters: {}", err);
+                        }
+                    }
+                });
+
+                // Bottom Results
+                if let Some(purple_reports) = self.reports.purple_reports {
+                    ui.separator();
+                    ui.label(format!(
+                        "Quantity of Orange Reports: {:.2}",
+                        purple_reports * 5.
+                    ));
+                    ui.label(format!("Quantity of Purple Reports: {:.2}", purple_reports));
+                }
+
+                if let Some(exp) = self.reports.exp {
+                    ui.label(format!("Quantity of EXP: {}", exp));
                 }
             });
-
-            // Buttons
-            ui.horizontal(|ui| {
-                if ui.button("Convert").clicked() {
-                    let purple_reports = (self.reports.quantities[0] / 200.0)
-                        + (self.reports.quantities[1] / 20.0)
-                        + (self.reports.quantities[2] / 5.0)
-                        + self.reports.quantities[3];
-                    let exp = purple_reports * 10000.0;
-                    self.reports.purple_reports = Some(purple_reports);
-                    self.reports.exp = Some(exp);
-                }
-
-                if ui.button("Clear").clicked() {
-                    self.reports.purple_reports = None;
-                    self.reports.exp = None;
-                }
-
-                if ui.button("Save").clicked() {
-                    let reports_path = "reports.json";
-                    let character_path = "characters.json";
-                    if let Err(err) = self.reports.save_to_file(reports_path) {
-                        eprintln!("Failed to save reports: {}", err);
-                    }
-                    if let Err(err) = self.character.save_to_file(character_path) {
-                        eprintln!("Failed to save characters: {}", err);
-                    }
-                }
-            });
-            // Bottom Results
-            if let Some(purple_reports) = self.reports.purple_reports {
-                ui.separator();
-                ui.label(format!(
-                    "Quantity of Orange Reports: {:.2}",
-                    purple_reports * 5.
-                ));
-                ui.label(format!("Quantity of Purple Reports: {:.2}", purple_reports));
-            }
-
-            if let Some(exp) = self.reports.exp {
-                ui.label(format!("Quantity of EXP: {}", exp));
-            }
         });
     }
 }
